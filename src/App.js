b4de192a8c437d58273f7ad196e7c6f0e52b57ec
unknown <egr_96@hotmail.com>
2020-01-27 09:39:03 -0300

    new components added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,43 @@
-import React from 'react';
-import './app.css';
+import React, { useState, useEffect } from "react";
+import ButtonRequestView from "./views/ButtonRequestView";
+import requestData from "./modules/helper";
+import PrhasesView from "./views/PhraseView";
+import { Container, ContainerPhrase } from "./assets/styled";
+import "./app.css";
 
 function App() {
+  const [randomQuoteId, setRandomQuoteId] = useState();
+  const [phrases, setPhrases] = useState([]);
+
+  let phrasesInLS = JSON.parse(localStorage.getItem("PHRASES", phrases));
+  if (!phrasesInLS) {
+    phrasesInLS = [];
+  }
+
+  useEffect(() => {
+    if (phrasesInLS.length < 1) {
+      requestData()
+        .then(phrasesArrayData =>
+          localStorage.setItem("PHRASES", JSON.stringify(phrasesArrayData))
+        )
+        .then(phrasesArrayData => setPhrases(phrasesArrayData));
+    } else {
+      console.log("no llamé a la api ");
+      setPhrases(phrasesInLS);
+    }
+  }, []);
+
   return (
-    <div className="App">
-    
-    </div>
+    <Container>
+      <ContainerPhrase>
+        <PrhasesView
+          phrasesData={phrases}
+          randomQuoteId={randomQuoteId}
+          setPhrases={setPhrases}
+        />
+      </ContainerPhrase>
+      <ButtonRequestView setRandomQuoteId={setRandomQuoteId} />
+    </Container>
   );
 }
 
